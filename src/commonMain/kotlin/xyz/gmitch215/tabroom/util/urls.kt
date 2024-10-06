ff670e8cb336b62e7b6c15a30e419367e8e8c99e
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-10-06 15:23:32 -0500

    Add `events` and `judges` URLs

--- src/commonMain/kotlin/xyz/gmitch215/tabroom/util/urls.kt
+++ src/commonMain/kotlin/xyz/gmitch215/tabroom/util/urls.kt
@@ -10,4 +10,9 @@ internal class TabroomUrls(
     val entries: String
         get() = "https://www.tabroom.com/index/tourn/fields.mhtml?tourn_id=$tournamentId"
 
+    val events: String
+        get() = "https://www.tabroom.com/index/tourn/events.mhtml?tourn_id=$tournamentId"
+
+    val judges: String
+        get() = "https://www.tabroom.com/index/tourn/judges.mhtml?tourn_id=$tournamentId"
 }
