13dd24f809ed79c2689e8f802315b28e17106679
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-10-13 19:32:46 -0500

    Add `body`, `head`, `bodyElements` Fields

--- src/commonMain/kotlin/xyz/gmitch215/tabroom/util/html/Document.kt
+++ src/commonMain/kotlin/xyz/gmitch215/tabroom/util/html/Document.kt
@@ -3,9 +3,20 @@ package xyz.gmitch215.tabroom.util.html
 internal class Document(
     val url: String,
     val html: String
-)
+) {
 
-internal fun String.toDocument(url: String): Document = Document(url, this)
+    val body: Element
+        get() = querySelector("body") ?: throw IllegalStateException("Document does not have a body element")
+
+    val bodyElements: String
+        get() = body.innerHTML
+            .replace(Regex("<script\\b[^>]*>([\\s\\S]*?)</script>"), "")
+            .replace(Regex("<style\\b[^>]*>([\\s\\S]*?)</style>"), "")
+
+    val head: Element
+        get() = querySelector("head") ?: throw IllegalStateException("Document does not have a head element")
+
+}
 
 internal expect fun Document.querySelectorAll(selector: String): List<Element>
 internal fun Document.querySelector(selector: String): Element? = querySelectorAll(selector).firstOrNull()
