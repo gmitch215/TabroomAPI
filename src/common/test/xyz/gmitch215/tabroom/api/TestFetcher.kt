16c8ae3234eba2b72442efa1529b4b5259dc7852
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-03-01 15:41:53 -0600

    Add Tests for New Features

--- src/common/test/xyz/gmitch215/tabroom/api/TestFetcher.kt
+++ src/common/test/xyz/gmitch215/tabroom/api/TestFetcher.kt
@@ -2,6 +2,7 @@ package xyz.gmitch215.tabroom.api
 
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 import kotlin.time.Duration.Companion.minutes
 
@@ -18,6 +19,15 @@ class TestFetcher {
         assertTrue(t1.location.isNotEmpty())
 
         assertTrue(t1.events.isNotEmpty())
+        assertTrue(t1.events.any { it.entries.any { it.ballots.isNotEmpty() } })
+
+        val firstBallot = t1.events.filter { it.entries.any { it.ballots.isNotEmpty() } }[0].entries[0].ballots.entries.first().value
+        assertTrue(firstBallot.eventName.isNotEmpty())
+        assertTrue(firstBallot.round > 0)
+        assertTrue(firstBallot.judge.isNotEmpty())
+        assertTrue(firstBallot.tournamentDate.isNotEmpty())
+        assertTrue(firstBallot.tournament.isNotEmpty())
+        assertTrue(firstBallot.speaker1Points > 0)
 
         // Schaumburg 2024
         val t2 = getTournament(32668)
@@ -28,6 +38,7 @@ class TestFetcher {
         assertTrue(t2.location.isNotEmpty())
 
         assertTrue(t2.events.isNotEmpty())
+        assertFalse(t2.events.any { it.entries.any { it.ballots.isNotEmpty() } })
     }
 
 }
