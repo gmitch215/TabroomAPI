b3d2a0fed80bf7f5f7cc0f61c072f3165cfd0ca8
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-06-07 01:19:55 -0500

    fix: Darwin SSL

--- src/apple/main/dev/gmitch215/tabroom/util/http.apple.kt
+++ src/apple/main/dev/gmitch215/tabroom/util/http.apple.kt
@@ -1,12 +1,22 @@
 package dev.gmitch215.tabroom.util
 
-import io.ktor.client.engine.*
-import io.ktor.client.engine.darwin.*
-import kotlinx.coroutines.Dispatchers
 
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.darwin.Darwin
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.UnsafeNumber
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import platform.Foundation.*
+import platform.Security.*
+
+@OptIn(ExperimentalForeignApi::class, UnsafeNumber::class)
 internal actual val engine: HttpClientEngine = Darwin.create {
     pipelining = true
-    dispatcher = Dispatchers.Default
+    dispatcher = Dispatchers.IO.limitedParallelism(PARALLEL_COUNT)
 
     configureRequest {
         setAllowsCellularAccess(true)
@@ -14,5 +24,20 @@ internal actual val engine: HttpClientEngine = Darwin.create {
 
     configureSession {
         setAllowsCellularAccess(true)
+    }
+
+    handleChallenge { session, task, challenge, completionHandler ->
+        val serverTrust = challenge.protectionSpace.serverTrust ?: run {
+            completionHandler(NSURLSessionAuthChallengeCancelAuthenticationChallenge, null)
+            return@handleChallenge
+        }
+
+        memScoped {
+            val resultVar = alloc<SecTrustResultTypeVar>()
+            SecTrustEvaluate(serverTrust, resultVar.ptr)
+        }
+
+        val credential = NSURLCredential.credentialForTrust(serverTrust)
+        completionHandler(NSURLSessionAuthChallengeUseCredential, credential)
     }
 }
