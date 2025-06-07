b4be5d50777430e1723b965b7eeccedaf9153615
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-06-07 01:20:15 -0500

    chore: switch to `Curl` for Linux

--- src/linux/main/dev/gmitch215/tabroom/util/http.linux.kt
+++ src/linux/main/dev/gmitch215/tabroom/util/http.linux.kt
@@ -1,11 +1,11 @@
 package dev.gmitch215.tabroom.util
 
-import io.ktor.client.engine.*
-import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.curl.Curl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 
-internal actual val engine: HttpClientEngine = CIO.create {
+internal actual val engine: HttpClientEngine = Curl.create {
     pipelining = true
     dispatcher = Dispatchers.IO.limitedParallelism(PARALLEL_COUNT)
 }
